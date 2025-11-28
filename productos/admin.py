f39bd44332761a29d91bc2d6c788a550b0a73c40
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-28 20:37:49 -0300

    configuracion

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Producto, Categoria, Etiqueta, DetallesProducto
+
+@admin.register(Producto)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'precio', 'categoria', 'creado_en')
+    list_filter = ('categoria', 'etiquetas')
+    search_fields = ('nombre', 'descripcion')
+    admin.site.register(Categoria)
+    admin.site.register(Etiqueta)
+    admin.site.register(DetallesProducto)
